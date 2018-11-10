ea7adf2ddda7d62d3297ac3d8878b750c853023b
Ogie Ben <bensoft2k5@gmail.com>
2018-11-10 10:03:12 +0100

    imported schema

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import rootSchema from './data/schema/rootSchema'
+import rootSchema from './data/schema/rootSchema';
 import  graphqlHttp  from 'express-graphql';
 
 
 const app = express();
 
 
-app.use('/graphql', graphQlHttp({schema: rootSchema}))
+app.use('/graphql', graphqlHttp({schema: rootSchema}));
 
 export default app;
