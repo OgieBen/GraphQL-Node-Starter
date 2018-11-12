b06b6c01fe4f6cbd9c08adba9cd938d05e12a3e6
Ogie Ben <bensoft2k5@gmail.com>
2018-11-12 14:29:59 +0100

    add repo handleTypeResolver

--- app/data/interfaces/ObjectInterface.js
+++ app/data/interfaces/ObjectInterface.js
@@ -1,8 +1,20 @@
-import { GraphQLInterface } from 'graphql';
+import { GraphQLInterface, GraphQLNonNull, GraphQLID } from 'graphql';
+import { Repo } from '../repo/Repo'
 
+let repo = new Repo();
 
 let ObjectInterface = new GraphQLInterface({
-    name: "ObjectInterface"
+    name: "ObjectInterface",
+    fields: {
+        id: {
+            type: new GraphQLNonNull(GraphQLID)
+        },
+
+        // source: current data
+        resolveType(source){
+            repo.handleTypeResolver(source);
+        }
+    }
 });
 
 
