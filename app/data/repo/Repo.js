5673fc3c805a633712e0d5c4a0d036c4dd23b303
Ogie Ben <bensoft2k5@gmail.com>
2018-11-12 14:34:08 +0100

    fixed typo in method

---
+++ app/data/repo/Repo.js
@@ -0,0 +1,20 @@
+export default class Repo {
+    constructor() {
+
+    }
+
+    /**
+     * Return table name (for sql) or document name (for nosql)
+     */
+    getDocumentName() {
+        return '';
+    }
+
+    /**
+     * Handles type resolution on ObjectInterface 
+     * @param {Object} source 
+     */
+    handleTypeResolver(source) {
+    
+    }
+}
